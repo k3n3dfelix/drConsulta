b2749af877d010d70bd5cfcb106067e0fda0e6f0
k3n3dfelix <kened.felix@gmail.com>
2025-05-30 00:41:29 -0300

    chore: add shadcn initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className=" flex-col gap-[32px] row-start-2 items-center sm:items-start flex">
-        <p>Hello World</p>
+        <p>Hello Worlds</p>
       </main>
       <footer className="row-start-3  gap-[24px] flex-wrap items-center flex justify-center"></footer>
     </div>
   );
 }
+
