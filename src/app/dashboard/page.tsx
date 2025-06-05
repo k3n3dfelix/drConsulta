cd4fee218d6de8c29343ba78b62a63620533489c
k3n3dfelix <kened.felix@gmail.com>
2025-06-04 21:27:36 -0300

    feat: finalizado regras de login

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,38 @@
-const DashboardPage = () => {
-  return ( <p>Dashboard</p> );
+'"use client";'
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+//import { auth } from "@/lib/auth";
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  //com ServerComponent 
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  //Com ClientComponent
+  // const session = authClient.useSession();
+  
+  if(!session?.user) {
+    redirect("/authentication")
+  }
+  return ( 
+    <>
+      <p>Dashboard</p> 
+       {/* com ServerComponent  */}
+      {/* <h1>{session?.user.name}</h1>
+      <h1>{session?.user.email}</h1> */}
+
+        {/* Com ClientComponent  */}
+      <h1>{session?.user.name}</h1>
+      <h1>{session?.user.email}</h1>
+      <SignOutButton />
+      
+    </>
+);
 }
  
 export default DashboardPage;
