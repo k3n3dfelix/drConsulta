a7d1271763c03a73f7bf543f41abfc1ebb4df0d3
k3n3dfelix <kened.felix@gmail.com>
2025-06-23 22:57:24 -0300

    feat: ajuste de utc no salvamento de horas

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,5 +1,7 @@
 
 "use server";
+import dayjs from 'dayjs';
+import utc from  'dayjs/plugin/utc';
 import { headers } from 'next/headers';
 
 import { db } from '@/db';
@@ -9,8 +11,22 @@ import { actionClient } from '@/lib/next-safe-action';
 
 import { upsertDoctorSchema } from './schema';
 
+dayjs.extend(utc);
+
 export const upsertDoctor = actionClient.schema(upsertDoctorSchema).action(async ({parsedInput}) => {
 
+  const availableFromTime = parsedInput.availableFromTime;
+  const availableToTime = parsedInput.availableToTime;
+
+  const availableFromTimeUtc = dayjs()
+  .set("hour",  parseInt(availableFromTime.split(':')[0]))
+  .set("minute",  parseInt(availableFromTime.split(':')[1]))
+  .set("second",  parseInt(availableFromTime.split(':')[2]));
+
+  const availableToTimeUtc = dayjs()
+  .set("hour",  parseInt(availableToTime.split(':')[0]))
+  .set("minute",  parseInt(availableToTime.split(':')[1]))
+  .set("second",  parseInt(availableToTime.split(':')[2]));
 
   upsertDoctorSchema.parse(parsedInput);
 
@@ -31,12 +47,15 @@ export const upsertDoctor = actionClient.schema(upsertDoctorSchema).action(async
     id: parsedInput.id,
     clinicId: session?.user.clinic?.id,
     ...parsedInput,
+    availableFromTime: availableFromTimeUtc.format('HH:mm:ss'),
+    availableToTime: availableToTimeUtc.format('HH:mm:ss'),
   })
   .onConflictDoUpdate({
     target: [doctorsTable.id],
     set: {
-      ...parsedInput
-    
+      ...parsedInput,
+      availableFromTime: availableFromTimeUtc.format('HH:mm:ss'),
+      availableToTime: availableToTimeUtc.format('HH:mm:ss'),
     },
   })
 })
